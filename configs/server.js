00306b8858fc90b2cce05ce5dd9fd06e68e71577
Informatica <informatica@alumno.local>
2025-02-04 15:31:29 -0600

    Programa avanzado

---
+++ configs/server.js
@@ -0,0 +1,45 @@
+"use strict"
+
+import express from 'express'
+import cors from "cors"
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './mongo.js'
+import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
+import apiLimiter from '../src/middlewares/validar-cant-peticiones.js'
+
+const middlewares = (app) =>{
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+    app.use(apiLimiter)
+}
+
+const routes = (app) => {
+    app.use("/adoptionSystem/v1/auth", authRoutes)
+    app.use("/adoptionSystem/v1/user", userRoutes)
+}
+
+const connectarDB = async () => {
+    try{
+        await dbConnection()
+    }catch(err){
+        console.log(`Database connection failed: ${err}`)
+    }
+}
+
+export const initServer = () =>{
+    const app = express()
+    try{
+        middlewares(app)
+        connectarDB()
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port: ${process.env.PORT}`)
+    }catch(err){
+        console.log(`Server init failed: ${err}`)
+    }
+}
---
+++ src/user/user.model.js
@@ -0,0 +1,58 @@
+import {Schema, model} from "mongoose";
+
+const userSchema = Schema({
+    name:{
+        type: String,
+        required: [true, "Name is required"],
+        maxLength:[25, "Name cannot exceed 25 characters"]
+    },
+    surname:{
+        type: String,
+        required: [true, "Name is required"],
+        maxLength:[25, "Name cannot exceed 25 characters"]
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    profilePicture:{
+        type: String
+    },
+    phone:{
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 8
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["ADMIN_ROLE", "USER_ROLE"]
+    },
+    status:{
+        type:Boolean,
+        default: true
+    }
+},
+{
+    versionkey: false,
+    timeStamps: true
+})
+
+userSchema.methods.toJSON = function(){
+    const { password, _id, ...user} = this.toObject()
+    user.uid = _id
+    return user
+}
+export default model("User", userSchema)
